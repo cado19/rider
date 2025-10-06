00169054cf40a951843dfb285dd2650a23811657
cado19 <mryoungrichie@gmail.com>
2025-10-06 18:16:26 +0300

    started trip_request

---
+++ app/(root)/trip_request.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function trip_request() {
+  return (
+    <View>
+      <Text>trip_request</Text>
+    </View>
+  )
+}
