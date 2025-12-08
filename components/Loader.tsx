fd1497c31383f393dee944eb221be206346caff2
cado19 <mryoungrichie@gmail.com>
2025-12-08 11:49:40 +0300

    loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+export default function Loader() {
+  return (
+    <View style={styles.centered}>
+        <ActivityIndicator size="large" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centered: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
