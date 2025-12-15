7eb417f08f42e282306a59b79a2a7eccfe3c9994
cado19 <mryoungrichie@gmail.com>
2025-12-15 12:42:31 +0300

    Added message to loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View, Text } from "react-native";
 
-export default function Loader() {
+export default function Loader({ message }: { message: string}) {
   return (
     <View style={styles.centered}>
         <ActivityIndicator size="large" />
+        <Text style={styles.message}>{message}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   centered: { flex: 1, justifyContent: "center", alignItems: "center" },
+  message: { marginTop: 16, fontSize: 18, fontWeight: "500" },
 });
