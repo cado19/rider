bbc76dcff47c65c74c71682d1a094b1da3ec36ed
cado19 <mryoungrichie@gmail.com>
2025-12-17 19:28:50 +0300

    added fonts and initialized

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot, Tabs } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen, Tabs } from 'expo-router'
 import { AuthProvider } from '../context/AuthProvider'
+import { useFonts } from "expo-font";
 
 export default function _layout() {
+  const [fontsLoaded] = useFonts({
+    JakartaRegular: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    JakartaMedium: require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    JakartaSemiBold: require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    JakartaBold: require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if(fontsLoaded){
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded){
+    return null;
+  }
+  
   return (
     <AuthProvider>
       <Slot />
@@ -11,3 +29,5 @@ export default function _layout() {
     
   )
 }
+
+
